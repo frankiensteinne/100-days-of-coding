e9cb64fe36df5550f85c1a40f6c57a785380b2df
frankiensteinne <nobleza.frankie@gmail.com>
2025-01-01 14:27:53 +0800

    Inputted day2  exercises and README

---
+++ day2/README.md
@@ -0,0 +1,7 @@
+# Day 2 of 100 : Understanding Data Types and How to Manipulate Strings
+- Strings and their properties.
+- String manipulation: changing case, concatenation.
+- f-strings for formatting.
+
+# Python Crash Course, 3rd Edition
+Pages 15-27
---
+++ day2/supplemental.py
@@ -0,0 +1,82 @@
+border = "*" * 10
+
+def print_border(level,item,status):
+    print(border,level,item,status, border)
+
+level = "Base"
+
+item = "Player Introduction"
+'''
+Task:  Write a program that takes a player's first and last name as input and prints a message like, "Welcome to the team, [first name] [last name]!". 
+Expected Output: "Welcome to the team, [first name] [last name]!"
+'''
+
+print_border(level,item, "Start")
+#Enter your code below this line
+
+print_border(level,item, "End")
+
+item = "Race Position"
+'''
+Task: Write a program that takes a driver's name and their finishing position as input and prints a message like, "[Driver's name] finished the race in [position] place!". 
+Expected Output: "[Driver's name] finished the race in [position] place!"
+'''
+
+print_border(level,item, "Start")
+#Enter your code below this line
+
+print_border(level,item, "End")
+
+
+level = "Comfortable"
+
+item = "Goal Celebration"
+'''
+Task: Write a program that takes a player's name and the number of goals they scored as input. Print a message like, "[Player's name] scored [number] goals! ¡Visca el Barça!". Use f-strings for formatting.
+
+Expected Output: "[Player's name] scored [number] goals! ¡Visca el Barça!"
+'''
+
+print_border(level,item, "Start")
+#Enter your code below this line
+
+print_border(level,item, "End")
+
+item = "Fastest Lap"
+'''
+Task: Write a program that takes a driver's name and their fastest lap time as input. Print a message like, "[Driver's name] set the fastest lap with a time of [lap time]!". Use f-strings for formatting.
+
+Expected Output: "[Driver's name] set the fastest lap with a time of [lap time]!"
+'''
+
+print_border(level,item, "Start")
+#Enter your code below this line
+
+print_border(level,item, "End")
+
+
+level = "More Comfortable"
+
+item = "Team Chant"
+'''
+Task: Write a program that takes a team name as input and creates a chant by repeating the team name three times, separated by commas and an exclamation mark at the end. Then, convert the chant to uppercase. (pp. 19-21)
+
+Expected Output: "[TEAM NAME], [TEAM NAME], [TEAM NAME]!" (all uppercase)
+'''
+
+print_border(level,item, "Start")
+#Enter your code below this line
+
+print_border(level,item, "End")
+
+item = "Driver Lineup"
+'''
+Task: Write a program that takes the names of three drivers as input. Create a formatted string that lists the drivers separated by " | " and adds " - 2025 F1 Season" at the end. (pp. 25-27)
+
+Expected Output: "[Driver 1] | [Driver 2] | [Driver 3] - 2025 F1 Season"
+'''
+
+print_border(level,item, "Start")
+#Enter your code below this line
+
+print_border(level,item, "End")
