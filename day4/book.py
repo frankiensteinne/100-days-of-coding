2ac9e05559ad858a5d695811e89923bac235b4ed
frankiensteinne <nobleza.frankie@gmail.com>
2025-01-03 01:20:08 +0800

    Init main.py file

--- day4/book.py
+++ day4/book.py
@@ -1,8 +1,23 @@
 #3-1. Names: Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a time.
 
+names = ["Mika","Claudia","Dau"]
+
+print(names[0])
+print(names[1])
+print(names[2])
+
 # 3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+greeting = "Hello,"
+for name in names:
+    print(greeting, name)
 
 # 3-3. Your Own List: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+sentence = "I would like to have my own"
+cars = ["Ferrari LaFerrari", "McLaren P1 Senna", "Bentley Continental", "Rolls-Royce Phantom"]
+
+for car in cars:
+    print(sentence, car)
 
 # 3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
 
---
+++ main.py
@@ -0,0 +1,36 @@
+f1_drivers = [
+    "Max Verstappen",
+    "Liam Lawson",
+    "Charles Leclerc",
+    "Lewis Hamilton",
+    "Lando Norris",
+    "Oscar Piastri",
+    "George Russell",
+    "Kimi Antonelli",
+    "Fernando Alonso",
+    "Lance Stroll",
+    "Yuki Tsunoda",
+    "Isack Hadjar",
+    "Oliver Bearman",
+    "Esteban Ocon",
+    "Pierre Gasly",
+    "Jack Doohan",
+    "Alex Albon",
+    "Carlos Sainz",
+    "Nico Hulkenberg",
+    "Gabriel Bortoleto"
+]
+
+starting_eleven = [
+    "Inaki Pena",
+    "Kounde",
+    "Pau Cubarsi",
+    "Inigo Martinez",
+    "Balde",
+    "Marc Casado",
+    "Pedri",
+    "Raphina",
+    "Gavi",
+    "Fermin Lopez",
+    "Robert Lewandowski"
+]
