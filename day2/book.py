b63da0bcda2a6dbf7f871f76ae3b95dc2c1131af
frankiensteinne <nobleza.frankie@gmail.com>
2025-01-02 19:58:41 +0800

    Finished Day 2

--- day2/book.py
+++ day2/book.py
@@ -1,7 +1,19 @@
 # 2-3. Personal Message: Use a variable to represent a person’s name, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 
+person_to_greet = "Eric"
+print(f"Hello, {person_to_greet}, would you like to learn some Python today?")
+
 # 2-4. Name Cases: Use a variable to represent a person’s name, and then print that person’s name in lowercase, uppercase, and title case.
+name_cases = "Frenkie de Jong"
+print(name_cases.lower())
+print(name_cases.upper())
+print(name_cases.title())
 
 # 2-5. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks: Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+print('Albert Einstein once said, “A person who never made a mistake never tried anything new.”')
 
 # 2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, represent the famous person’s name using a variable called famous_person. Then compose your message and represent it with a new variable called message. Print your message.
+famous_person = "Albert Einstein"
+quote = 'A person who never made a mistake never tried anything new.'
+message = f'{famous_person} once said, "{quote}"'
+print(message)
--- day2/supplemental.py
+++ day2/supplemental.py
@@ -13,6 +13,10 @@ Expected Output: "Welcome to the team, [first name] [last name]!"
 
 print_border(level,item, "Start")
 #Enter your code below this line
+first_name = input("What is your first name?:\n\t").title()
+last_name= input("What is your last name?:\n\t").title()
+
+print(f"Welcome to the team, {first_name} {last_name}!")
 
 print_border(level,item, "End")
 
@@ -24,7 +28,10 @@ Expected Output: "[Driver's name] finished the race in [position] place!"
 
 print_border(level,item, "Start")
 #Enter your code below this line
+driver_name = input("What is the driver's name?: ").title()
+ordinal_position = input("Where did he placed?: ")
 
+print(f"{driver_name} finished the race in {ordinal_position} place!")
 print_border(level,item, "End")
 
 
@@ -39,6 +46,10 @@ Expected Output: "[Player's name] scored [number] goals! ¡Visca el Barça!"
 
 print_border(level,item, "Start")
 #Enter your code below this line
+player_name = input("Input player's name: ").title()
+goals = input("How many goals did they scored?: ")
+
+print(f"{player_name} scored {goals} goals!\n¡Visca el Barça!")
 
 print_border(level,item, "End")
 
@@ -51,6 +62,10 @@ Expected Output: "[Driver's name] set the fastest lap with a time of [lap time]!
 
 print_border(level,item, "Start")
 #Enter your code below this line
+driver_name = input("What is the driver's name?: ").title()
+lap_time = input("Input lap time in seconds: ")
+
+print(f"{driver_name} set the fastest lap with a time of {lap_time} seconds!")
 
 print_border(level,item, "End")
 
@@ -59,24 +74,30 @@ level = "More Comfortable"
 
 item = "Team Chant"
 '''
-Task: Write a program that takes a team name as input and creates a chant by repeating the team name three times, separated by commas and an exclamation mark at the end. Then, convert the chant to uppercase. (pp. 19-21)
+Task: Write a program that takes a team name as input and creates a chant by repeating the team name three times, separated by commas and an exclamation mark at the end. Then, convert the chant to uppercase.
 
 Expected Output: "[TEAM NAME], [TEAM NAME], [TEAM NAME]!" (all uppercase)
 '''
 
 print_border(level,item, "Start")
 #Enter your code below this line
+team_name = input("What is your team's name?: ").upper()
 
+print(f'{team_name}, {team_name}, {team_name}!')
 print_border(level,item, "End")
 
 item = "Driver Lineup"
 '''
-Task: Write a program that takes the names of three drivers as input. Create a formatted string that lists the drivers separated by " | " and adds " - 2025 F1 Season" at the end. (pp. 25-27)
+Task: Write a program that takes the names of three drivers as input. Create a formatted string that lists the drivers separated by " | " and adds " - 2025 F1 Season" at the end.
 
 Expected Output: "[Driver 1] | [Driver 2] | [Driver 3] - 2025 F1 Season"
 '''
 
 print_border(level,item, "Start")
 #Enter your code below this line
+driver1 = input("Input driver name: ").title()
+driver2 = input("Input driver name: ").title()
+driver3 = input("Input driver name: ").title()
 
+print(f'{driver1} | {driver2} | {driver3} - 2025 F1 Season')
 print_border(level,item, "End")
