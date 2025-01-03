cfba24dcc468771a6aec1beffcc6a9a431c46fe3
frankiensteinne <nobleza.frankie@gmail.com>
2025-01-03 22:17:42 +0800

    uncommented exercise 4-4

--- day5/book.py
+++ day5/book.py
@@ -33,8 +33,8 @@ for number in range(1,21):
 
 
 # 4-4. One Million: Make a list of the numbers from one to one million, and then use a for loop to print the numbers. (If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)
-# for number in range(1,1000001):
-#    print(number)
+for number in range(1,1000001):
+    print(number)
 
 
 # 4-5. Summing a Million: Make a list of the numbers from one to one million, and then use min() and max() to make sure your list actually starts at one and ends at one million. Also, use the sum() function to see how quickly Python can add a million numbers.
