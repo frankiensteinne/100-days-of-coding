f9b6d32966fb49252e6fb3147b8d123c460b27f8
frankiensteinne <nobleza.frankie@gmail.com>
2025-01-03 18:49:50 +0800

    Init day 5 files

---
+++ day5/supplemental.py
@@ -0,0 +1,129 @@
+border = "*" * 10
+
+def print_border(level,item,status):
+    print(border,level,item,status, border)
+    
+def linebreak():
+    print("\n")
+
+level = "Base"
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+level = "Comfortable"
+
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+level = "More Comfortable"
+
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
