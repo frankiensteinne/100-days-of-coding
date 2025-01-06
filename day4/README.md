d20eefea573ff067d110606479a9d41ad2f3bd6d
frankiensteinne <nobleza.frankie@gmail.com>
2025-01-06 20:40:50 +0800

    Init day4 files

---
+++ day4/README.md
@@ -0,0 +1,13 @@
+# Day 4 of 100: Conditional Statements and Logic
+
+- `if`, `elif`, and `else` Statements
+- Logical Operators (`and`, `or`, `not`)
+- Comparison Operators (`==`, `!=`, `<`, `>`, etc.)
+
+## TL;DR:
+
+Conditionals are essential for decision-making in Python. You’ll learn to write programs that respond to user input or data dynamically.
+
+## Python Crash Course, 3rd Edition:
+
+Pages 41–50 (focus on “Conditional Tests” and “If Statements”).
---
+++ day4/exercise.py
@@ -0,0 +1,135 @@
+border = "*" * 10
+
+def print_border(level,item,status):
+    print(border,level,item,status, border)
+    
+def linebreak():
+    print("\n")
+
+level = "Base"
+item = "Driver Qualifier"
+'''
+Concepts: if Statements, Comparison Operators
+Task: Write a program that checks if a driver’s lap time is under 80 seconds.
+Expected Output:
+Lap time: 78.2  
+Output: Qualifies for the next round!  
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+item = "Fan Eligibility"
+'''
+Concepts: if-else Statements
+Task: Ask the user for their age and check if they are eligible for discounted Barça tickets (under 18 or over 60).
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+item = "Team Comparison"
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+level = "Comfortable"
+
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+level = "More Comfortable"
+
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
+
+item = ""
+'''
+
+'''
+
+print_border(level,item, "Start")
+linebreak()
+#Enter your code below this line
+
+linebreak()
+print_border(level,item, "End")
+linebreak()
