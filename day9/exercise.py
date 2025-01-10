bd7cea1f6fbd1b2e649606544d8d8fd74b2c4dc3
frankiensteinne <nobleza.frankie@gmail.com>
2025-01-10 23:06:10 +0800

    finished day 9

--- day9/exercise.py
+++ day9/exercise.py
@@ -22,6 +22,11 @@ driver_stats = {
     "points": 180
     }
 
+for key, value in driver_stats.items():
+    key = key.title()
+    if type(value) == 'str':
+        value = value.title()
+    print(f"{key}: {value}")
 
 linebreak()
 print_border(level,item, "End")
@@ -38,7 +43,9 @@ linebreak()
 #Enter your code below this line
 team_goals = {"Lewandowski": 10, "Pedri": 5}
 
+team_goals["Ferran Torres"] = 3
 
+print(team_goals)
 linebreak()
 print_border(level,item, "End")
 linebreak()
@@ -58,6 +65,9 @@ driver_stats = {
     "points": 180
     }
 
+driver_stats["points"] = 220
+
+print(driver_stats)
 linebreak()
 print_border(level,item, "End")
 linebreak()
@@ -74,6 +84,14 @@ linebreak()
 #Enter your code below this line
 driver_names = ["Carlos Sainz", "Charles Leclerc", "Lewis Hamilton"]  
 lap_times = [78.2, 79.5, 80.3]  
+
+def create_lap_times(drivers, times):
+    driver_dict = {key: value for key, value in zip(drivers,times)}
+    
+    return driver_dict
+    
+new_dict = create_lap_times(driver_names, lap_times)
+print(new_dict)
 
 
 linebreak()
@@ -94,6 +112,13 @@ team_goals = {
     "Ferran Torres": 3
     }
 
+def sort_team_goals(dictionary_of_players):
+    sorted_list = sorted(dictionary_of_players.items(), key=lambda x: x[1], reverse=True)
+    return sorted_list
+
+
+sorted_team_goals = sort_team_goals(team_goals)
+print(sorted_team_goals)
 
 linebreak()
 print_border(level,item, "End")
@@ -109,6 +134,21 @@ Task: Write a function check_driver_points(driver_stats) that:
 print_border(level,item, "Start")
 linebreak()
 #Enter your code below this line
+
+def check_driver_pts(dictionary_of_driver_stats):
+    status =""
+    if 200 < dictionary_of_driver_stats["points"]:
+        status = "Leader"
+    elif 150 < dictionary_of_driver_stats["points"] <= 200:
+        status = "Contender"
+    else:
+        status = "Midfield"
+    
+    return status
+
+positions = check_driver_pts(driver_stats)
+print(driver_stats["points"])
+print(positions)        
 
 linebreak()
 print_border(level,item, "End")
@@ -128,7 +168,15 @@ linebreak()
 #Enter your code below this line
 players = ["Lewandowski", "Pedri", "Ferran Torres"]  
 goals = [10, 5, 3]  
-assists = [7, 8, 4]  
+assists = [7, 8, 4] 
+
+def create_goal_report(players,goal_tally, assist_tally):
+    goal_assist_dict = {a:{"goals":b, "assists":c} for (a,b,c) in zip(players, goal_tally, assist_tally)}
+    return goal_assist_dict
+
+GA_dict = create_goal_report(players, goals, assists)
+
+print(GA_dict)
 
 linebreak()
 print_border(level,item, "End")
@@ -141,13 +189,21 @@ Task: Write a function analyze_driver_performance(driver_stats) that:
 - Checks if a driver’s points are above 250 and adds a "status" key with the value "Champion Contender".
 - If points are between 150–250, sets "status" to "Contender".
 Below 150, sets "status" to "Midfield".
-
 '''
-
 print_border(level,item, "Start")
 linebreak()
 #Enter your code below this line
-
+def analyze_driver_performance(driver_stats):
+    if 250 < driver_stats["points"]:
+        driver_stats["status"] = "Champion Contender"
+    elif 150 < driver_stats["points"] <= 250:
+        driver_stats["status"] = "Contender"
+    else:
+        driver_stats["status"] = "Midfield"
+    return driver_stats
+    
+updated_driver_dict = analyze_driver_performance(driver_stats)
+print(updated_driver_dict)
 linebreak()
 print_border(level,item, "End")
 linebreak()
@@ -165,8 +221,27 @@ print_border(level,item, "Start")
 linebreak()
 #Enter your code below this line
 teams = ["Barça", "Real Madrid", "Atletico Madrid"]  
-scores = [3, 1, 1]  
+scores = [3, 0, 1]
 
+def check_results(points):
+    results = []
+    for value in points:
+        if value == 3:
+           results.append('Win')
+        elif value == 1:
+            results.append('Draw')
+        else:
+            results.append('Lose')
+    return results
+
+
+def match_results_summary(team_list, points_list, results_list):
+    match_summary = {a:{'points':b, 'results':c} for (a,b,c) in zip(team_list, points_list, results_list)}
+    return match_summary
+    
+results = check_results(scores)
+summary__dictionary = match_results_summary(teams,scores,results)
+print(summary__dictionary)
 
 linebreak()
 print_border(level,item, "End")
